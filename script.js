1e6dd41bad56cbc892d03734db418e011ab9ed7e
Naseem Akbar <101437439+naseem1471@users.noreply.github.com>
2024-02-21 03:53:38 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,76 +1,74 @@
 var swiper = new Swiper('.Slider-container', {
-  effect: 'cards',
-  grabCursor: true,
-  centerdSlides: true,
-  loop: true,
-});
+    effect: 'cards',
+    grabCursor: true,
+    centerdSlides: true,
+    loop: true,
+  });
 // swiper.changeDirection('vertical');
 
 // Declare a global variable for the chosenSlider using let
 let chosenSlider = null;
 
+
+// Get the form element
+let form = document.getElementById("my-form");
 // Get the buttons
 const reject = document.getElementById("reject");
 const accept = document.getElementById("accept");
-// Get the form element
-let form = document.getElementById("my-form");
-
 
 // Add event listeners to the buttons
 reject.addEventListener("click", function () {
-  // Get the image element of the current slide
-  var img = document.querySelector(".swiper-slide:nth-child(" + (swiper.activeIndex + 1) + ") img");
-  // Set the opacity of the image to 0.3
-  img.style.opacity = 0.05;
-  // Move to the next slide
-  swiper.slideNext();
-});
-accept.addEventListener("click", function () {
-  // Get the image element of the current slide
-  var img = document.querySelector(".swiper-slide:nth-child(" + (swiper.activeIndex + 1) + ") img");
-  // Set the opacity of the image to 1
-  img.style.opacity = 1;
-  // Move to the next slide
-  swiper.slideNext();
-});
+    // Get the image element of the current slide
+    var img = document.querySelector(".swiper-slide:nth-child(" + (swiper.activeIndex + 1) + ") img");
+    // Set the opacity of the image to 0.3
+    img.style.opacity = 0.05;
+    // Move to the next slide
+    var slideId = document.querySelector(".swiper-slide.swiper-slide-active").getAttribute("data-id");
+    var square = document.querySelector(".square[data-id='" + slideId + "']");
+    square.style.backgroundColor = "grey";
+    swiper.slideNext();
+  });
+  accept.addEventListener("click", function () {
+    // Get the image element of the current slide
+    var img = document.querySelector(".swiper-slide:nth-child(" + (swiper.activeIndex + 1) + ") img");
+    // Set the opacity of the image to 1
+    img.style.opacity = 1;
+    // Set the background color of the square to white
+    var slideId = document.querySelector(".swiper-slide.swiper-slide-active").getAttribute("data-id");
+    var square = document.querySelector(".square[data-id='" + slideId + "']");
+    square.style.backgroundColor = "#FFD700";
+    // Move to the next slide
+    swiper.slideNext();
+  });
 
 // Add an event listener for the submit event
 form.addEventListener("submit", function(event) {
-  // Prevent the default form action
-  event.preventDefault();
-
-  // Get the input value
-  let chosen = form.elements["chosen"].value;
-
-  // Assign the input value to the global variable
-  chosenSlider = chosen;
-
-   // Save chosenSlider to localStorage
-   localStorage.setItem("chosenSlider", chosenSlider);
-
-  // Get the result element
-  let result = document.getElementById("result");
-
-  // Display the result using a template string
-  result.textContent = `The player has chosen image ${chosenSlider}`;
-
-  // Update the src attribute of the image inside .image-container
-  let imageContainer = document.querySelector(".image-container img");
-  imageContainer.src = `images/img_${chosenSlider}.jpg`;
-
+    // Prevent the default form action
+    event.preventDefault();
   
+    // Get the input value
+    let chosen = form.elements["chosen"].value;
   
-  // Hide the form and result paragraph
+    // Assign the input value to the global variable
+    chosenSlider = chosen;
+  
+     // Save chosenSlider to localStorage
+     localStorage.setItem("chosenSlider", chosenSlider);
+  
+    // Get the result element
+    let result = document.getElementById("result");
+  
+    // Display the result using a template string
+    result.textContent = `The player has chosen image ${chosenSlider}`;
+  
+    // Update the src attribute of the image inside .image-container
+    let imageContainer = document.querySelector(".containerImage img");
+    imageContainer.src = `images/img_${chosenSlider}.jpg`;
+
+    // Hide the form and result paragraph
   form.style.display = "none";
-  result.style.display = "none";
+  
    
 
   console.log(chosenSlider);
 });
-
-//console.log(chosenSlider);//
-
-
-
-
-
